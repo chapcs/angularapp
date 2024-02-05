a3d0b1e91dd84431b3b2b2961aaec54e23afa847
Ben Chapman <93676414+chapcs@users.noreply.github.com>
2024-02-05 23:29:06 +0000

    Fixed build errors and removed NGModule so that each component is standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Hello World!</h1>',
-  styleUrls: ['./app.component.css'], 
+  template: '<main><header class="brand-name"><img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"></header><section class="content"><app-home></app-home></section></main>',
+  //imports: [ HomeComponent, ],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
